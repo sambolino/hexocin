00293635b0207d9b70256f554b4d836ef8bed4f9
Veljko Vujcic <sambolino@gmail.com>
2020-10-21 14:12:06 +0200

    moved data files to data/ folder

--- parse_data.py
+++ parse_data.py
@@ -1,48 +1,7 @@
 import re
 import sqlite3
 from sqlite3 import Error
-
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return conn
-
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
-
-def populate_table(conn, row):
-    """
-    Create a new project into the projects table
-    :param conn:
-    :param project:
-    :return: project id
-    """
-    sql = ''' INSERT INTO data_points(d,p,data)
-              VALUES(?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, row)
-    conn.commit()
-    return cur.lastrowid
+from sqlite_utils import *
 
 def chunks(l, n):
     """ split list l to chunks based on size of chunk n """
@@ -73,7 +32,7 @@ def main():
             list_of_dicts.append(dic)
 
     #print(list_of_dicts)
-    database = r"mixing_agn.db"
+    database = r"/mixing_agn.db"
     conn = create_connection(database)
 
     sql_create_table = """ CREATE TABLE IF NOT EXISTS data_points (
