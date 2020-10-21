b001eff3b66aabfa5129d12e622f32a501824dcd
Veljko Vujcic <sambolino@gmail.com>
2020-10-21 17:40:15 +0200

    range functions returning value

--- mixing_parser.py
+++ mixing_parser.py
@@ -3,27 +3,27 @@ import argparse
 from calculate_and_plot import *
 
 def _n_range(val):
-    ival = int(val)
-    if val not in range (4, 20):
+    if int(val) not in range (4, 21):
         raise argparse.ArgumentTypeError("n value should be between 4 and 20")
+    return int(val)
 
 def _p_range(val):
-    ival = int(val)
-    if val not in range (1, 5):
+    if int(val) not in range (1, 6):
         raise argparse.ArgumentTypeError("p value should be between 1 and 5")
+    return int(val)
 
 def _t_range(val):
-    ival = int(val)
-    if val not in range (1, 50000):
+    if int(val) not in range (1, 50001):
         raise argparse.ArgumentTypeError("t value should be between 1 and 50000")
+    return int(val)
 
 PARSER = argparse.ArgumentParser(description="", 
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-PARSER.add_argument("-n", type=_n_range, default=None, help="aaa", required=True)
-PARSER.add_argument("-p", type=_p_range, default=None, help="aaa", required=True)
-PARSER.add_argument("-t", type=_t_range, default=None, help="aaa", required=True)
+PARSER.add_argument("-n", "--n", type=_n_range, default=None, help="aaa", required=True)
+PARSER.add_argument("-p", "--p", type=_p_range, default=None, help="aaa", required=True)
+PARSER.add_argument("-t", "--temp", type=_t_range, default=None, help="aaa", required=True)
 ARGS = PARSER.parse_args()
 
 if __name__ == "__main__":
-    sys.exit(calculate_and_plot(ARGS.n, ARGS.p, ARGS.t))
+    sys.exit(calculate_and_plot(ARGS.n, ARGS.p, ARGS.temp))
